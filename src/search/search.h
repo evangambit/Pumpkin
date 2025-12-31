86d9567ef03dd47e33b831eec73a009b62b74f46
evangambit <evangambit@gmail.com>
2025-12-30 22:57:23 -0800

    Add multipv support

--- src/search/search.h
+++ src/search/search.h
@@ -16,30 +16,33 @@ namespace ChessEngine {
 
 struct Thread {
   uint64_t id_;
+  uint64_t multiPV_;
   Position position_;
   std::shared_ptr<EvaluatorInterface> evaluator_;
   std::unordered_set<Move> permittedMoves_;
+  std::vector<std::pair<Move, Evaluation>> primaryVariations_;  // Contains multiPV number of best moves.
   uint64_t nodeCount_{0};
 
   Thread(
     uint64_t id,
     const Position& pos,
     std::shared_ptr<EvaluatorInterface> evaluator,
+    uint64_t multiPV,
     const std::unordered_set<Move>& permittedMoves
   )
-    : id_(id), position_(pos), evaluator_(evaluator), permittedMoves_(permittedMoves) {}
+    : id_(id), position_(pos), evaluator_(evaluator), permittedMoves_(permittedMoves), multiPV_(multiPV) {}
   std::atomic<bool> stopSearchFlag{false};
 };
 
 template<Color TURN>
-struct SearchResult {
-  SearchResult() : bestMove(kNullMove), evaluation(0) {}
-  SearchResult(Move move, Evaluation eval) : bestMove(move), evaluation(ColoredEvaluation<TURN>(eval)) {}
+struct NegamaxResult {
+  NegamaxResult() : bestMove(kNullMove), evaluation(0) {}
+  NegamaxResult(Move move, Evaluation eval) : bestMove(move), evaluation(ColoredEvaluation<TURN>(eval)) {}
   Move bestMove;
   ColoredEvaluation<TURN> evaluation;
 
-  SearchResult<opposite_color<TURN>()> operator-() const {
-    return SearchResult<opposite_color<TURN>()>(bestMove, -evaluation);
+  NegamaxResult<opposite_color<TURN>()> operator-() const {
+    return NegamaxResult<opposite_color<TURN>()>(bestMove, -evaluation);
   }
 };
 
@@ -58,13 +61,13 @@ enum SearchType {
 };
 
 template<Color TURN>
-SearchResult<TURN> qsearch(Thread* thread, ColoredEvaluation<TURN> alpha, ColoredEvaluation<TURN> beta, int plyFromRoot) {
+NegamaxResult<TURN> qsearch(Thread* thread, ColoredEvaluation<TURN> alpha, ColoredEvaluation<TURN> beta, int plyFromRoot) {
   // TODO: Implement quiescence search
-  return SearchResult<TURN>(kNullMove, evaluate<TURN>(thread->evaluator_, thread->position_).value);
+  return NegamaxResult<TURN>(kNullMove, evaluate<TURN>(thread->evaluator_, thread->position_).value);
 }
 
 template<Color TURN, SearchType SEARCH_TYPE>
-SearchResult<TURN> negamax(Thread* thread, int depth, ColoredEvaluation<TURN> alpha, ColoredEvaluation<TURN> beta, int plyFromRoot) {
+NegamaxResult<TURN> negamax(Thread* thread, int depth, ColoredEvaluation<TURN> alpha, ColoredEvaluation<TURN> beta, int plyFromRoot) {
   if (depth == 0) {
     return qsearch(thread, alpha, beta, plyFromRoot);
   }
@@ -98,26 +101,49 @@ SearchResult<TURN> negamax(Thread* thread, int depth, ColoredEvaluation<TURN> al
 
   if (moves == end) {
     if (inCheck) {
-      return SearchResult<TURN>(kNullMove, kCheckmate + plyFromRoot);
+      return NegamaxResult<TURN>(kNullMove, kCheckmate + plyFromRoot);
     } else {
-      return SearchResult<TURN>(kNullMove, 0);
+      return NegamaxResult<TURN>(kNullMove, 0);
     }
   }
 
   // We need to check this *after* we do the checkmate test above, since you can win on the 50th move.
   if (thread->position_.is_fifty_move_rule()) {
-    return SearchResult<TURN>(kNullMove, 0);
+    return NegamaxResult<TURN>(kNullMove, 0);
   }
 
-  SearchResult<TURN> bestResult(kNullMove, kMinEval);
+  NegamaxResult<TURN> bestResult(kNullMove, kMinEval);
   for (ExtMove* move = moves; move < end; ++move) {
     make_move<TURN>(&thread->position_, move->move);
     ColoredEvaluation<TURN> eval = -negamax<opposite_color<TURN>(), SearchType::NORMAL_SEARCH>(thread, depth - 1, -beta, -alpha, plyFromRoot + 1).evaluation;
     undo<TURN>(&thread->position_);
     if (eval.value > bestResult.evaluation.value) {
-      bestResult = SearchResult<TURN>(move->move, eval.value);
+      bestResult = NegamaxResult<TURN>(move->move, eval.value);
       if (bestResult.evaluation.value > alpha.value) {
-        alpha = ColoredEvaluation<TURN>(bestResult.evaluation.value);
+        if (SEARCH_TYPE == SearchType::ROOT && thread->multiPV_ > 1) {
+          // In multi-PV search, we want to keep track of multiple best moves and
+          // only raise alpha if we have the top N moves.
+
+          // We don't really care about optimizing this too much since it only happens
+          // at the root of the search.
+          thread->primaryVariations_.push_back(std::make_pair(move->move, eval.value));
+          std::sort(
+            thread->primaryVariations_.begin(),
+            thread->primaryVariations_.end(),
+            [](const std::pair<Move, Evaluation>& a, const std::pair<Move, Evaluation>& b) {
+              return a.second > b.second;
+            }
+          );
+          if (thread->primaryVariations_.size() >= thread->multiPV_) {
+            alpha = ColoredEvaluation<TURN>(thread->primaryVariations_[thread->multiPV_ - 1].second);
+            if (thread->primaryVariations_.size() > thread->multiPV_) {
+              thread->primaryVariations_.pop_back();
+            }
+          }
+        } else {
+          // If we're not at the root, just update alpha.
+          alpha = ColoredEvaluation<TURN>(bestResult.evaluation.value);
+        }
       }
       if (alpha.value >= beta.value) {
         break;
@@ -128,11 +154,40 @@ SearchResult<TURN> negamax(Thread* thread, int depth, ColoredEvaluation<TURN> al
   return bestResult;
 }
 
-inline std::pair<ColoredEvaluation<Color::WHITE>, Move> search(Position pos, std::shared_ptr<EvaluatorInterface> evaluator, int depth) {
+struct SearchResult {
+  std::vector<std::pair<Move, ColoredEvaluation<Color::WHITE>>> primaryVariations;
+  Move bestMove;
+  ColoredEvaluation<Color::WHITE> evaluation;
+};
+
+inline SearchResult search(Position pos, std::shared_ptr<EvaluatorInterface> evaluator, int depth, int multiPV = 1) {
   pos.set_listener(evaluator);
-  Thread thread(0, pos, evaluator, std::unordered_set<Move>());
-  SearchResult<Color::WHITE> result = negamax<Color::WHITE, SearchType::ROOT>(&thread, depth, ColoredEvaluation<Color::WHITE>(kMinEval), ColoredEvaluation<Color::WHITE>(kMaxEval), 0);
-  return {result.evaluation, result.bestMove};
+  Thread thread(0, pos, evaluator, multiPV, std::unordered_set<Move>());
+  if (pos.turn_ == Color::WHITE) {
+    NegamaxResult<Color::WHITE> result = negamax<Color::WHITE, SearchType::ROOT>(
+      &thread, depth,
+      /*alpha=*/ColoredEvaluation<Color::WHITE>(kMinEval),
+      /*beta=*/ColoredEvaluation<Color::WHITE>(kMaxEval),
+      /*plyFromRoot=*/0
+    );
+    std::vector<std::pair<Move, ColoredEvaluation<Color::WHITE>>> convertedPVs;
+    for (const auto& pv : thread.primaryVariations_) {
+      convertedPVs.push_back(std::make_pair(pv.first, ColoredEvaluation<Color::WHITE>(pv.second)));
+    }
+    return SearchResult{convertedPVs, result.bestMove, result.evaluation};
+  } else {
+    NegamaxResult<Color::BLACK> result = negamax<Color::BLACK, SearchType::ROOT>(
+      &thread, depth,
+      /*alpha=*/ColoredEvaluation<Color::BLACK>(kMinEval),
+      /*beta=*/ColoredEvaluation<Color::BLACK>(kMaxEval),
+      /*plyFromRoot=*/0
+    );
+    std::vector<std::pair<Move, ColoredEvaluation<Color::WHITE>>> convertedPVs;
+    for (const auto& pv : thread.primaryVariations_) {
+      convertedPVs.push_back(std::make_pair(pv.first, -ColoredEvaluation<Color::BLACK>(pv.second)));
+    }
+    return SearchResult{convertedPVs, result.bestMove, -result.evaluation};
+  }
 }
 
 }  // namespace ChessEngine
