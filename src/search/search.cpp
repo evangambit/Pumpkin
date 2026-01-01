66405cb2d7be44dfa0c28d82103768068a6a5ea6
evangambit <evangambit@gmail.com>
2025-12-31 19:16:22 -0800

    x

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -1,6 +1,38 @@
 #include "search.h"
-#include "transposition_table.h"
 
 namespace ChessEngine {
+
+SearchResult<Color::WHITE> colorless_search(Thread* thread, int depth, std::function<void(int, SearchResult<Color::WHITE>)> onDepthCompleted = nullptr) {
+  if (thread->position_.turn_ == Color::WHITE) {
+    return search<Color::WHITE>(thread, depth, onDepthCompleted);
+  } else {
+    if (onDepthCompleted != nullptr) {
+      std::function<void(int, SearchResult<Color::BLACK>)> wrappedOnDepthCompleted =
+        [onDepthCompleted](int depth, SearchResult<Color::BLACK> resultBlack) {
+          SearchResult<Color::WHITE> resultWhite = -resultBlack;
+          onDepthCompleted(depth, resultWhite);
+        };
+      return -search<Color::BLACK>(thread, depth, wrappedOnDepthCompleted);
+    }
+    else {
+      return -search<Color::BLACK>(thread, depth, nullptr);
+    }
+  }
+}
+
+// Convenience function to search programmatically without needing to specify color or create a thread.
+SearchResult<Color::WHITE> search(Position pos, std::shared_ptr<EvaluatorInterface> evaluator, int depth, int multiPV, TranspositionTable* tt) {
+  pos.set_listener(evaluator);
+  tt->new_search();
+  Thread thread(0, pos, evaluator, multiPV, std::unordered_set<Move>());
+  thread.tt_ = tt;
+
+  if (pos.turn_ == Color::WHITE) {
+    return search<Color::WHITE>(&thread, depth, nullptr);
+  } else {
+    SearchResult<Color::BLACK> result = search<Color::BLACK>(&thread, depth, nullptr);
+    return -result;
+  }
+}
 
 }  // namespace ChessEngine
