ade7d3875c692bb10539d2f810cf493aa2ad7a1e
evangambit <evangambit@gmail.com>
2026-02-07 17:54:47 -0800

    x

--- src/game/tests/GeometryTests.cpp
+++ src/game/tests/GeometryTests.cpp
@@ -24,6 +24,10 @@ TEST_F(GeometryTest, NorthFill) {
   EXPECT_BB_EQ(northFill(bb(SA2) | bb(SC4)), bb(SA2) | bb(SA3) | bb(SA4) | bb(SA5) | bb(SA6) | bb(SA7) | bb(SA8) | bb(SC4) | bb(SC5) | bb(SC6) | bb(SC7) | bb(SC8));
 }
 
+TEST_F(GeometryTest, SouthFill) {
+  EXPECT_BB_EQ(southFill(bb(SA7) | bb(SC5)), bb(SA1) | bb(SA2) | bb(SA3) | bb(SA4) | bb(SA5) | bb(SA6) | bb(SA7) | bb(SC1) | bb(SC2) | bb(SC3) | bb(SC4) | bb(SC5));
+}
+
 TEST_F(GeometryTest, Shift) {
   // Simple shifts
   EXPECT_BB_EQ(shift<Direction::NORTH>(bb(SE4)), bb(SE5));
@@ -35,6 +39,11 @@ TEST_F(GeometryTest, Shift) {
   EXPECT_BB_EQ(shift<Direction::WEST>(bb(SA8)), kEmptyBitboard);
   EXPECT_BB_EQ(shift<Direction::EAST>(bb(SH1)), kEmptyBitboard);
   EXPECT_BB_EQ(shift<Direction::SOUTH>(bb(SH1)), kEmptyBitboard);
+  // Full board tests.
+  EXPECT_BB_EQ(shift_ew<Direction::EAST>(kUniverse, 1), kUniverse & ~kFiles[0]);
+  EXPECT_BB_EQ(shift_ew<Direction::WEST>(kUniverse, 1), kUniverse & ~kFiles[7]);
+  EXPECT_BB_EQ(shift<Direction::NORTH>(kUniverse), kUniverse & ~kRanks[7]);
+  EXPECT_BB_EQ(shift<Direction::SOUTH>(kUniverse), kUniverse & ~kRanks[0]);
 }
 
 TEST_F(GeometryTest, Shift_EW) {
@@ -44,6 +53,11 @@ TEST_F(GeometryTest, Shift_EW) {
   // Masking
   EXPECT_BB_EQ(shift_ew<Direction::EAST>(bb(SE2) | bb(SF2), 3), bb(SH2));
   EXPECT_BB_EQ(shift_ew<Direction::WEST>(bb(SC2) | bb(SD2), 3), bb(SA2));
+  // Full board tests.
+  EXPECT_BB_EQ(shift_ew<Direction::EAST>(kUniverse, 1), kUniverse & ~kFiles[0]);
+  EXPECT_BB_EQ(shift_ew<Direction::EAST>(kUniverse, 2), kUniverse & ~kFiles[0] & ~kFiles[1]);
+  EXPECT_BB_EQ(shift_ew<Direction::WEST>(kUniverse, 1), kUniverse & ~kFiles[7]);
+  EXPECT_BB_EQ(shift_ew<Direction::WEST>(kUniverse, 2), kUniverse & ~kFiles[7] & ~kFiles[6]);
 }
 
 TEST_F(GeometryTest, FlipVertically) {
