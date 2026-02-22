62ace383d49d853b064548ab4861da74bb53540c
Morgan Redding <evangambit@gmail.com>
2026-02-22 06:07:15 -0800

    Increase embedding dim to 1024

--- src/eval/nnue/Utils.h
+++ src/eval/nnue/Utils.h
@@ -39,7 +39,7 @@ int16_t flip_feature_index(int16_t index);
 
 // Shift used for int16_t fixed-point scaling.
 constexpr int SCALE_SHIFT = 8;
-constexpr int EMBEDDING_DIM = 512;
+constexpr int EMBEDDING_DIM = 1024;
 constexpr int HIDDEN1_DIM = 64;
 constexpr int OUTPUT_DIM = 1;
 
