d935546829f881f9a5cb592ff5b5a747ecf48b94
Morgan Redding <evangambit@gmail.com>
2026-02-22 04:39:37 -0800

    Increase hidden layer from 16 to 64

--- model_data.cpp
+++ model_data.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d57599050668ae7b2fd868330d79bdb405c7caf46a62556dd9bbe07e4211cf0
-size 19804494
+oid sha256:1ea56bb06518edfffff55ffc55f1c9932d805b67f13d65c2aa7363c351744f3c
+size 21019278
