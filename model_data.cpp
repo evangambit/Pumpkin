20f7ab830cd4919ee141781637aa9f24bd5a59ee
Morgan Redding <evangambit@gmail.com>
2026-03-02 01:58:08 -0800

    Expand first layer to 1024 (Elo difference: 143.1 +/- 48.8, LOS: 100.0 %, DrawRatio: 12.0 %)

--- model_data.cpp
+++ model_data.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ee952cefc231e1e3e65afe77bdb71f8c8959ed689c644a97d944750a1a1e103
-size 20209422
+oid sha256:8f6ac4ef2b9cf98f2ac411e28b4c56446075391aafff3874980d996b3681f486
+size 40416354
