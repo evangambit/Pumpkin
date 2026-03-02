89af6077c54377a0f816dd170ad601d6890ff808
Morgan Redding <evangambit@gmail.com>
2026-03-01 23:27:06 -0800

    Bump up hidden layer from 16 to 32 dimensions.
    This is basically free given the parallelization, and boosts Elo by 200-300 points

--- model_data.cpp
+++ model_data.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f55711be8ea312ea7dd6df68718de54637408f2d0fcc13993fb4096bcd8458e
-size 19804494
+oid sha256:7ee952cefc231e1e3e65afe77bdb71f8c8959ed689c644a97d944750a1a1e103
+size 20209422
