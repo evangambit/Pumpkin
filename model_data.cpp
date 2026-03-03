e4dbd92ab00fab0f475d7c2c11f22c122231430a
Morgan Redding <evangambit@gmail.com>
2026-03-03 00:39:16 -0800

    Train on 300M positions (~+300 Elo)

--- model_data.cpp
+++ model_data.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f6ac4ef2b9cf98f2ac411e28b4c56446075391aafff3874980d996b3681f486
+oid sha256:a0712ca3abb3481a0781cd9d01998bde912aaaa526aff4ee2411af56bb33e126
 size 40416354
