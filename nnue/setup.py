6cf902244c41c261d9f7c48720903cb454507e4c
Morgan Redding <evangambit@gmail.com>
2026-03-03 23:34:42 -0800

    Enable O3 and NDEBUG in setup.py

--- nnue/setup.py
+++ nnue/setup.py
@@ -3,7 +3,16 @@ import sys
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
-extra_compile_args = ['-std=c++20']
+# Add -O3 for optimization and -DNDEBUG to disable assertions
+extra_compile_args = ['-std=c++20', '-O3', '-DNDEBUG']
+
+if sys.platform == 'darwin':
+    extra_compile_args.append('-Wno-invalid-specialization')
+    # Use Apple Clang on macOS
+    if not os.environ.get("CC"):
+        os.environ["CC"] = "/usr/bin/clang"
+    if not os.environ.get("CXX"):
+        os.environ["CXX"] = "/usr/bin/clang++"
 
 if sys.platform == 'darwin':
     extra_compile_args.append('-Wno-invalid-specialization')
