b16940a9845936b698264b94e9cb56fb6dba95eb
Morgan Redding <evangambit@gmail.com>
2026-03-04 05:08:03 -0800

    Fix train.py

--- nnue/train.py
+++ nnue/train.py
@@ -58,144 +58,127 @@ BATCH_SIZE = 2048
 CHUNK_SIZE = 128
 assert BATCH_SIZE % CHUNK_SIZE == 0
 
-# Create a directory for this run
-timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-run_dir = os.path.join("runs", timestamp)
-os.makedirs(run_dir, exist_ok=True)
-
-device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
-
-print("Loading dataset...")
-dataset = ndata.NnueDataset(['../data/pos.shuf.txt'])
-
-print(f'Dataset loaded with {len(dataset) * CHUNK_SIZE} rows.')
-
 def collate_fn(rows):
-  features, labels, piece_counts = zip(*rows)
-  values, lengths = zip(*features)
+  values, lengths, labels, turns = zip(*rows)
   values = torch.from_numpy(np.concatenate(values))
   lengths = torch.from_numpy(np.concatenate(lengths))
   labels = torch.from_numpy(np.stack(labels))
-  piece_counts = torch.from_numpy(np.stack(piece_counts))
+  turns = torch.from_numpy(np.stack(turns))
   labels = labels.reshape(labels.shape[0] * labels.shape[1], *labels.shape[2:])
-  piece_counts = piece_counts.reshape(piece_counts.shape[0] * piece_counts.shape[1], *piece_counts.shape[2:])
-  return values, lengths, labels, piece_counts
-
-dataloader = tdata.DataLoader(dataset, batch_size=BATCH_SIZE//CHUNK_SIZE, shuffle=False, num_workers=0, pin_memory=True, drop_last=True, collate_fn=collate_fn)
-
-print("Creating model...")
- # [512, 128]
-model = NNUE(hidden_sizes=[512, 8], output_size=1).to(device)
-
-with open('model.pt', 'rb') as f:
-  model.load_state_dict(torch.load(f, map_location=device))
-
-with open('model.bin', 'wb') as f:
-  save_tensor(model.emb.weight(model.emb.merged_tiles())[:-1], 'embedding', f)
-  save_tensor(model.mlp[0].weight, 'linear0.weight', f)
-  save_tensor(model.mlp[0].bias, 'linear0.bias', f)
-  save_tensor(model.mlp[2].weight, 'linear1.weight', f)
-  save_tensor(model.mlp[2].bias, 'linear1.bias', f)
+  turns = turns.reshape(turns.shape[0] * turns.shape[1], *turns.shape[2:])
+  return values, lengths, labels, turns
 
 
-exit(0)
+if __name__ == "__main__":
 
-print("Creating optimizer...")
-opt = torch.optim.AdamW(model.parameters(), lr=0.0, weight_decay=0.1)
+  # Create a directory for this run
+  timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+  run_dir = os.path.join("runs", timestamp)
+  os.makedirs(run_dir, exist_ok=True)
 
-def warmup_length(beta, c = 2.0):
-  # The amount of warmup needs to increase as beta approaches 1,
-  # since we need to see more data before the moving averages stabilize
-  # to its long-run variability.
-  return int(c / (1 - beta))
+  device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
 
-# Calculate total steps
-NUM_EPOCHS = 1
-steps_per_epoch = len(dataloader)
-total_steps = NUM_EPOCHS * steps_per_epoch
-warmup_steps = warmup_length(0.9) # AdamW's beta is 0.999.
-assert warmup_steps < total_steps // 10, "You probably made a mistake."
+  print("Loading dataset...")
+  dataset = ndata.NnueDataset(['../data/pos.shuf.txt'])
 
-scheduler = CosineAnnealingWithWarmup(
-  opt,
-  max_lr=3e-3,
-  min_lr=1e-5,
-  warmup_steps=warmup_steps,
-  total_steps=total_steps
-)
+  print(f'Dataset loaded with {len(dataset) * CHUNK_SIZE} rows.')
 
-def wdl2score(win_mover_perspective, draw_mover_perspective, lose_mover_perspective):
-  assert len(win_mover_perspective.shape) == 1
-  assert len(draw_mover_perspective.shape) == 1
-  assert len(lose_mover_perspective.shape) == 1
-  assert win_mover_perspective.shape == draw_mover_perspective.shape
-  assert win_mover_perspective.shape == lose_mover_perspective.shape
-  return win_mover_perspective + draw_mover_perspective * 0.5
+  dataloader = tdata.DataLoader(dataset, batch_size=BATCH_SIZE//CHUNK_SIZE, shuffle=False, num_workers=0, pin_memory=True, drop_last=True, collate_fn=collate_fn)
 
-metrics = defaultdict(list)
-for epoch in range(NUM_EPOCHS):
-  print(f"Starting Epoch {epoch+1}/{NUM_EPOCHS}")
-  for batch_idx, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
-    opt.zero_grad()
-    
-    # Update learning rate
-    scheduler.step()
-    
-    batch = [x.to(device) for x in batch]
-    values, lengths, y_white_perspective, piece_counts = batch
-    y_white_perspective = y_white_perspective.float() / 1000.0
+  print("Creating model...")
+  model = NNUE(hidden_sizes=[512, 8], output_size=1).to(device)
 
-    win_mover_perspective = y_white_perspective[:,0]
-    draw_mover_perspective = y_white_perspective[:,1]
-    lose_mover_perspective = y_white_perspective[:,2]
+  print("Creating optimizer...")
+  opt = torch.optim.AdamW(model.parameters(), lr=0.0, weight_decay=0.1)
 
-    output, layers = model(values, lengths)
+  def warmup_length(beta, c = 2.0):
+    # The amount of warmup needs to increase as beta approaches 1,
+    # since we need to see more data before the moving averages stabilize
+    # to its long-run variability.
+    return int(c / (1 - beta))
 
-    penalty = 0.0
-    for layer_output in layers:
-      penalty += (layer_output.mean() ** 2 + (layer_output.std() - 1.0) ** 2)
+  # Calculate total steps
+  NUM_EPOCHS = 1
+  steps_per_epoch = len(dataloader)
+  total_steps = NUM_EPOCHS * steps_per_epoch
+  warmup_steps = warmup_length(0.9) # AdamW's beta is 0.999.
+  assert warmup_steps < total_steps // 10, "You probably made a mistake."
 
-    output = torch.sigmoid(output)[:,0]
+  scheduler = CosineAnnealingWithWarmup(
+    opt,
+    max_lr=3e-3,
+    min_lr=1e-5,
+    warmup_steps=warmup_steps,
+    total_steps=total_steps
+  )
 
-    label = wdl2score(win_mover_perspective, draw_mover_perspective, lose_mover_perspective)
-    assert output.shape == label.shape, f"{output.shape} vs {label.shape}"
-    loss = nn.functional.mse_loss(
-      output, label, reduction='mean',
-    )
-    mse = loss.item()
-    baseline = ((label - label.mean()) ** 2).mean().item()
+  def wdl2score(win_mover_perspective, draw_mover_perspective, lose_mover_perspective):
+    assert len(win_mover_perspective.shape) == 1
+    assert len(draw_mover_perspective.shape) == 1
+    assert len(lose_mover_perspective.shape) == 1
+    assert win_mover_perspective.shape == draw_mover_perspective.shape
+    assert win_mover_perspective.shape == lose_mover_perspective.shape
+    return win_mover_perspective + draw_mover_perspective * 0.5
 
-    (loss + penalty * 0.02).backward()
-    opt.step()
-    metrics["loss"].append(loss.item())
-    metrics["mse"].append(mse / baseline)
-    metrics["penalty"].append(penalty.item())
-    if (batch_idx + 1) % 500 == 0:
-      print(f"loss: {np.mean(metrics['loss'][-1000:]):.4f}, mse: {np.mean(metrics['mse'][-1000:]):.4f}, penalty: {np.mean(metrics['penalty'][-1000:]):.4f}")
+  metrics = defaultdict(list)
+  for epoch in range(NUM_EPOCHS):
+    print(f"Starting Epoch {epoch+1}/{NUM_EPOCHS}")
+    for batch_idx, batch in tqdm(enumerate(dataloader), total=len(dataloader)):
+      opt.zero_grad()
+      
+      # Update learning rate
+      scheduler.step()
+      
+      batch = [x.to(device) for x in batch]
+      values, lengths, label, turn = batch
 
-# Save the model
+      output, layers = model(values, lengths)
 
-with open(os.path.join(run_dir, 'model.pt'), 'wb') as f:
-  torch.save(model.state_dict(), f)
+      penalty = 0.0
+      for layer_output in layers:
+        penalty += (layer_output.mean() ** 2 + (layer_output.std() - 1.0) ** 2)
 
-with open(os.path.join(run_dir, 'model.bin'), 'wb') as f:
-  save_tensor(model.emb.weight(model.emb.merged_tiles())[:-1], 'embedding', f)
-  save_tensor(model.mlp[0].weight, 'linear0.weight', f)
-  save_tensor(model.mlp[0].bias, 'linear0.bias', f)
-  save_tensor(model.mlp[2].weight, 'linear1.weight', f)
-  save_tensor(model.mlp[2].bias, 'linear1.bias', f)
+      output = torch.sigmoid(output)[:,0]
+      label = torch.sigmoid(label)
 
-plt.figure(figsize=(10,10))
-output = output.squeeze().cpu().detach().numpy()
-label = label.squeeze().cpu().detach().numpy()
-I = np.argsort(output)
-output, label = output[I], label[I]
-plt.scatter(output, label, alpha=0.1)
-plt.scatter(np.convolve(output, np.ones(100)/100, mode='valid'), np.convolve(label, np.ones(100)/100, mode='valid'), color='red', label='moving average')
-plt.savefig(os.path.join(run_dir, 'nnue-scatter.png'))
+      assert output.shape == label.shape, f"{output.shape} vs {label.shape}"
+      loss = nn.functional.mse_loss(
+        output, label, reduction='mean',
+      )
+      mse = loss.item()
+      baseline = ((label - label.mean()) ** 2).mean().item()
 
-plt.figure(figsize=(10,10))
-plt.plot(np.convolve(metrics['loss'][500:], np.ones(100)/100, mode='valid'), label='loss')
-plt.legend()
-plt.savefig(os.path.join(run_dir, 'nnue-loss.png'))
+      (loss + penalty * 0.02).backward()
+      opt.step()
+      metrics["loss"].append(loss.item())
+      metrics["mse"].append(mse / baseline)
+      metrics["penalty"].append(penalty.item())
+      if (batch_idx + 1) % 500 == 0:
+        print(f"loss: {np.mean(metrics['loss'][-1000:]):.4f}, mse: {np.mean(metrics['mse'][-1000:]):.4f}, penalty: {np.mean(metrics['penalty'][-1000:]):.4f}")
+
+  # Save the model
+
+  with open(os.path.join(run_dir, 'model.pt'), 'wb') as f:
+    torch.save(model.state_dict(), f)
+
+  with open(os.path.join(run_dir, 'model.bin'), 'wb') as f:
+    save_tensor(model.emb.weight(model.emb.merged_tiles())[:-1], 'embedding', f)
+    save_tensor(model.mlp[0].weight, 'linear0.weight', f)
+    save_tensor(model.mlp[0].bias, 'linear0.bias', f)
+    save_tensor(model.mlp[2].weight, 'linear1.weight', f)
+    save_tensor(model.mlp[2].bias, 'linear1.bias', f)
+
+  plt.figure(figsize=(10,10))
+  output = output.squeeze().cpu().detach().numpy()
+  label = label.squeeze().cpu().detach().numpy()
+  I = np.argsort(output)
+  output, label = output[I], label[I]
+  plt.scatter(output, label, alpha=0.1)
+  plt.scatter(np.convolve(output, np.ones(100)/100, mode='valid'), np.convolve(label, np.ones(100)/100, mode='valid'), color='red', label='moving average')
+  plt.savefig(os.path.join(run_dir, 'nnue-scatter.png'))
+
+  plt.figure(figsize=(10,10))
+  plt.plot(np.convolve(metrics['loss'][500:], np.ones(100)/100, mode='valid'), label='loss')
+  plt.legend()
+  plt.savefig(os.path.join(run_dir, 'nnue-loss.png'))
 
